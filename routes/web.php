89538e47f65d1028a2d8dae9399de86a0f14dd46
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2022-02-03 17:31:38 +0600

    profile controller created and some bug fixed

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AdminProfileController;
+use Illuminate\Support\Facades\Artisan;
 
 
 Auth::routes();
@@ -11,4 +13,18 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('/');
     Route::get('/home', [AdminController::class, 'index'])->name('home');
     Route::get('/starter', [AdminController::class, 'starter'])->name('starter');
+    // Profile Routes
+    Route::get('/profile/{id}', [AdminProfileController::class, 'index'])->name('profile.view');
+
+});
+
+// clear-all-from-web
+Route::get("clear", function (){
+    Artisan::call("cache:clear");
+    Artisan::call('optimize');
+    Artisan::call('route:cache');
+    Artisan::call('route:clear');
+    Artisan::call('view:clear');
+    Artisan::call('config:cache');
+    dd("all-clear");
 });
