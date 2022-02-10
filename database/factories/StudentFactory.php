deae89aa214b11e3fbf895e3f3598cb608236ed4
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2022-02-10 15:01:21 +0600

    factory edited

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -17,7 +17,7 @@ class StudentFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
-            'avatar' => 'public/backend/admin/images/default.png',
+            'avatar' => 'backend/admin/images/default.png',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
     }
